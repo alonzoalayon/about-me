85123d9d5e9b41b73b920402306f2acb399dc55f
alonzoalayon <alonzo@campgladiator.com>
2016-05-04 20:57:24 -0500

    update index and route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    pic: "https://avatars1.githubusercontent.com/u/16580180?v=3&amp;s=460",
+    bio: "I'm a party goer and like to jump off buildings.  Everything is nice and dandy"
+  });
 });
 
 module.exports = router;
